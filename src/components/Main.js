d936c1412b746300e94aef128bc26381a5f49c92
kesaviwebsolutions <89696473+mynkbhati22@users.noreply.github.com>
2022-09-01 17:13:36 +0530

    updated-main

--- src/components/Main.js
+++ src/components/Main.js
@@ -19,30 +19,33 @@ import {
   MDBTable,
   MDBTableHead,
   MDBTableBody,
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBBadge,
 } from "mdb-react-ui-kit";
 import { Container } from "@mui/system";
 import { getBlocks } from "./Blockchain/Web3-api";
+import { Grid } from "@mui/material";
 
 export default function Main() {
+  const [docBlock, setDocBlock] = useState();
 
-  const [docBlock, setDocBlock] = useState()
-
-  useEffect(()=>{
-    const init = async()=> {
+  useEffect(() => {
+    const init = async () => {
       const data = await getBlocks();
       setDocBlock(data);
-    }
+    };
     init();
-  },[])
-
+  }, []);
 
   const [showBasic, setShowBasic] = useState(false);
 
-  const slicingHash =(str)=>{
-    const first = str.slice(0,10);
+  const slicingHash = (str) => {
+    const first = str.slice(0, 10);
     const second = str.slice(56);
     return first + "..." + second;
-  }
+  };
 
   return (
     // NAVBAR
@@ -119,30 +122,151 @@ export default function Main() {
           </MDBCollapse>
         </MDBContainer>
       </MDBNavbar>
+
+      {/* CARDS */}
+      <Container maxWidth="xl">
+        <Grid
+          container
+          spacing={2}
+          sx={{ marginTop: "20px", padding: "10px 20px" }}
+        >
+          <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+            <MDBCard className="mx-5">
+              <MDBCardBody>
+                <MDBCardTitle>GCS Market Cap</MDBCardTitle>
+                <h4
+                  style={{
+                    fontWeight: " 500",
+                    color: "#000000",
+                    fontSize: "20px",
+                  }}
+                >
+                  hello
+                  <MDBBadge color="success" className="mx-2">
+                    {/* <MDBIcon fas icon="chart-line" /> 70.32% */}
+                  </MDBBadge>
+                </h4>
+              </MDBCardBody>
+            </MDBCard>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+            <MDBCard className="mx-5">
+              <MDBCardBody>
+                <MDBCardTitle>GCS Market Cap</MDBCardTitle>
+                <h4
+                  style={{
+                    fontWeight: " 500",
+                    color: "#000000",
+                    fontSize: "20px",
+                  }}
+                >
+                  hello
+                  <MDBBadge color="success" className="mx-2">
+                    {/* <MDBIcon fas icon="chart-line" /> 70.32% */}
+                  </MDBBadge>
+                </h4>
+              </MDBCardBody>
+            </MDBCard>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+            <MDBCard className="mx-5">
+              <MDBCardBody>
+                <MDBCardTitle>GCS Market Cap</MDBCardTitle>
+                <h4
+                  style={{
+                    fontWeight: " 500",
+                    color: "#000000",
+                    fontSize: "20px",
+                  }}
+                >
+                  hello
+                  <MDBBadge color="success" className="mx-2">
+                    {/* <MDBIcon fas icon="chart-line" /> 70.32% */}
+                  </MDBBadge>
+                </h4>
+              </MDBCardBody>
+            </MDBCard>
+          </Grid>{" "}
+          <Grid item xs={12} sm={12} md={6} lg={3} xl={3}>
+            <MDBCard className="mx-5">
+              <MDBCardBody>
+                <MDBCardTitle>GCS Market Cap</MDBCardTitle>
+                <h4
+                  style={{
+                    fontWeight: " 500",
+                    color: "#000000",
+                    fontSize: "20px",
+                  }}
+                >
+                  hello
+                  <MDBBadge color="success" className="mx-2">
+                    {/* <MDBIcon fas icon="chart-line" /> 70.32% */}
+                  </MDBBadge>
+                </h4>
+              </MDBCardBody>
+            </MDBCard>
+          </Grid>
+        </Grid>
+      </Container>
+
       {/* TABLE */}
-      <Container maxWidth="lg">
-        <MDBTable style={{ marginTop: "50px", border: "1px solid" }}>
-          <MDBTableHead>
-            <tr>
-              <th scope="col">Block Number</th>
-              <th scope="col">Txn</th>
-              <th scope="col">Gas Limit</th>
-              <th scope="col">Time</th>
-              <th scope="col">Hash</th>
-            </tr>
-          </MDBTableHead>
-          <MDBTableBody>
-           { docBlock && docBlock.map((item)=>{
-           return <tr>
-              <th scope="row">{item.number}</th>
-              <td>{item.txn}</td>
-              <td>{item.gasLimit}</td>
-              <td>{item.timestamp}</td>
-              <td>{slicingHash(item.hash)}</td>
-            </tr>
-           })}
-          </MDBTableBody>
-        </MDBTable>
+      <Container maxWidth="xl">
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={12} md={12} xl={6}>
+            <MDBTable style={{ marginTop: "50px", border: "1px solid" }}>
+              <MDBTableHead>
+                <tr>
+                  <th scope="col">Block Number</th>
+                  <th scope="col">Txn</th>
+                  <th scope="col">Gas Limit</th>
+                  <th scope="col">Time</th>
+                  <th scope="col">Hash</th>
+                </tr>
+              </MDBTableHead>
+              <MDBTableBody>
+                {docBlock &&
+                  docBlock.map((item) => {
+                    return (
+                      <tr>
+                        <th scope="row">{item.number}</th>
+                        <td>{item.txn}</td>
+                        <td>{item.gasLimit}</td>
+                        <td>{item.timestamp}</td>
+                        <td>{slicingHash(item.hash)}</td>
+                      </tr>
+                    );
+                  })}
+              </MDBTableBody>
+            </MDBTable>
+          </Grid>
+          <Grid item xs={12} sm={12} md={12} xl={6}>
+            <MDBTable style={{ marginTop: "50px", border: "1px solid" }}>
+              <MDBTableHead>
+                <tr>
+                  <th scope="col">Block Number</th>
+                  <th scope="col">Txn</th>
+                  <th scope="col">Gas Limit</th>
+                  <th scope="col">Time</th>
+                  <th scope="col">Hash</th>
+                </tr>
+              </MDBTableHead>
+              <MDBTableBody>
+                {docBlock &&
+                  docBlock.map((item) => {
+                    return (
+                      <tr>
+                        <th scope="row">{item.number}</th>
+                        <td>{item.txn}</td>
+                        <td>{item.gasLimit}</td>
+                        <td>{item.timestamp}</td>
+                        <td>{slicingHash(item.hash)}</td>
+                      </tr>
+                    );
+                  })}
+              </MDBTableBody>
+            </MDBTable>
+          </Grid>
+        </Grid>
       </Container>
     </>
   );
